af0127f69ec0e687f79badb56cbb4800dd3f57bc
Andrew <andrewnaquino@gmail.com>
2026-04-23 15:30:39 -0400

    initial commit

---
+++ cube.h
@@ -0,0 +1,14 @@
+//
+// Created by andrew on 4/22/26.
+//
+
+#ifndef CUBEBOT_CUBE_H
+#define CUBEBOT_CUBE_H
+#include <stdint.h>
+
+typedef struct cube {
+    uint64_t edges;
+    uint64_t corners;
+}cube;
+
+#endif //CUBEBOT_CUBE_H
